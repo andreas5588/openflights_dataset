43190d6c4bd333b5e70e48eff5482e8242cd2e7c
Andreas Schneider <san@ixdb.de>
2021-12-29 19:12:25 +0100

    Initial check in

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # openflights_dataset
 Openflights dataset, datamodel for InterSystems IRIS
+
+## InterSystems IRIS - LOAD DATA 
+
+https://docs.intersystems.com/iris20212/csp/docbook/DocBook.UI.Page.cls?KEY=RSQL_loaddata
+
+## Datasets
+This Dataset is inspited by https://community.intersystems.com/post/technology-bonuses-intersystems-iris-datasets-contest-2021
+
+
+Dataset formats: https://openflights.org/data.html#airline
+
+airlines.dat
+containing information on airlines
+
+routes.dat	
+Route file contains routes between airports on airlines spanning the globe
+
+planes.dat
+The OpenFlights plane file contains a curated selection of 173 passenger aircraft with IATA and/or ICAO codes, covering the vast majority of flights operated today and commonly used in flight schedules and reservation systems.
+	
+
+Dataset formats: https://ourairports.com/help/data-dictionary.html
+
+airports.csv 
+Each row in this dataset represents the record for a single airport. The primary key for interoperability purposes with other datasets is ident, but the actual internal OurAirports primary key is id. iso_region is a foreign key into the regions.csv download file. 
+
+airport_freq.csv 
+Each row in this dataset represents a single airport radio frequency for voice communication (radio navigation aids appear in navaids.csv). The column airport_ident is a foreign key referencing the ident column in airports.csv for the associated airport. 
+
+countries.csv 
+Each row represents a country or country-like entity (e.g. Hong Kong). The iso_country column in airports.csv, navaids.csv, and regions.csv refer to the code column here. 
+
+runways.csv 
+Each row in this dataset represents a single airport landing surface (runway, helipad, or waterway). The initial fields apply to the entire surface, in both directions. Fields beginning with le_* apply only to the low-numbered end of the runway (e.g. 09), while fields beginning with he_* apply only to the high-numbered end of the runway (e.g. 27). 
+
+navaids.csv 
+Each row in this dataset represents a single radio navigation. When the navaid is associated with an airport, the associated_airport field links to the ident field in airports.csv. 
+	
+regions.csv 
+Each row represents a high-level administrative subdivision of a country. The iso_region column in airports.csv links to the code column in this dataset. 
+
+## Data sources
+Raw data used to compute flight route geometries:
+
+#### Airports
+http://ourairports.com/data/
+
+#### Routes
+http://openflights.org/data.html
+
+#### Airport total passengers
+http://www.aci.aero/
+http://en.wikipedia.org/wiki/List_of_the_world%27s_busiest_airports_by_passenger_traffic#2014_statistics
+
+
+
+## repos
+https://github.com/jpatokal/openflights
+https://github.com/jpatokal/openflights/blob/master/sql/create.sql
+https://openflights.org/data.html#airline
+
+## Docker
+
+docker cp <src-path> <container>:<dest-path> ... 
+
+docker cp <container>:<src-path> <local-dest-path>
+
+docker cp D:\dev\openflights_dataset\data\airlines.dat my-iris:/usr/airlines.dat
+docker cp D:\dev\openflights_dataset\data\airports.csv my-iris:/usr/airports.csv
+docker cp D:\dev\openflights_dataset\data\countries.csv my-iris:/usr/countries.csv
+docker cp D:\dev\openflights_dataset\data\airport_freq.csv my-iris:/usr/airport_freq.csv
+docker cp D:\dev\openflights_dataset\data\regions.csv my-iris:/usr/regions.csv
+docker cp D:\dev\openflights_dataset\data\runways.csv my-iris:/usr/runways.csv
+docker cp D:\dev\openflights_dataset\data\navaids.csv my-iris:/usr/navaids.csv
+docker cp D:\dev\openflights_dataset\data\planes.dat my-iris:/usr/planes.dat
+docker cp D:\dev\openflights_dataset\data\routes.dat my-iris:/usr/routes.dat
---
+++ src/create_tables.sql
@@ -0,0 +1,501 @@
+
+--https://docs.intersystems.com/iris20212/csp/docbook/DocBook.UI.Page.cls?KEY=RSQL_loaddata
+
+CREATE DATABASE openflights
+GO
+USE DATABASE openflights
+GO
+/*
+Airline ID 	Unique OpenFlights identifier for this airline.
+Name 		Name of the airline.
+Alias 		Alias of the airline. For example, All Nippon Airways is commonly known as "ANA".
+IATA 		2-letter IATA code, if available.
+ICAO 		3-letter ICAO code, if available.
+Callsign 		Airline callsign.
+Country 		Country or territory where airport is located. See Countries to cross-reference to ISO 3166-1 codes.
+Active 		"Y" if the airline is or has until recently been operational, "N" if it is defunct. This field is not reliable: in particular, major airlines that stopped flying long ago, but have not had their IATA code reassigned (eg. Ansett/AN), will incorrectly show as "Y".
+*/
+DROP TABLE flights.airlines 
+GO
+CREATE TABLE flights.airlines 
+(
+  uid      int NOT NULL,
+  name     varchar(255),
+  alias    varchar(255),
+  iata     varchar(2) default NULL,
+  icao     varchar(3) default NULL,
+  callsign varchar(255),
+  country  varchar(255),
+  active   char(1) default 'N',   
+  CONSTRAINT uid_pk PRIMARY KEY (uid)
+)
+LOAD DATA FROM FILE '/usr/airlines.dat' 
+COLUMNS 
+(
+  uid      int,
+  name     varchar(255),
+  alias    varchar(255),
+  iata     varchar(2),
+  icao     varchar(3),
+  callsign varchar(255),
+  country  varchar(255),
+  active   char(1)
+)
+INTO flights.airlines
+(
+  uid,
+  name,
+  alias,
+  iata,
+  icao,
+  callsign,
+  country,
+  active
+) 
+VALUES 
+(
+  uid,
+  name,
+  alias,
+  iata,
+  icao,
+  callsign,
+  country,
+  active
+)
+USING {"from": {"file": {"header":"0","columnseparator":","} }}
+GO
+DROP TABLE flights.airports 
+GO
+CREATE TABLE flights.airports 
+(
+  id int NOT NULL,
+  ident varchar(8),
+  type varchar(25) NOT NULL,
+  name varchar(255),
+  latitude_deg DOUBLE,
+  longitude_deg DOUBLE,
+  elevation_ft int,
+  continent varchar(2),
+  iso_country varchar(2),
+  iso_region varchar(255),
+  municipality varchar(255),
+  scheduled_service bit,
+  gps_code varchar(4),
+  iata_code varchar(3),
+  local_code varchar(255),
+  home_link varchar(255),
+  wikipedia_link varchar(1024),
+  keywords varchar(1024),
+  CONSTRAINT id_pk PRIMARY KEY (id)
+)
+GO
+LOAD DATA FROM FILE '/usr/airports.csv' 
+INTO flights.airports
+(
+  id,
+  ident,
+  type,
+  name,
+  latitude_deg,
+  longitude_deg,
+  elevation_ft,
+  continent,
+  iso_country,
+  iso_region,
+  municipality,
+  scheduled_service,
+  gps_code,
+  iata_code,
+  local_code,
+  home_link,
+  wikipedia_link,
+  keywords
+) 
+VALUES 
+(
+  id,
+  ident,
+  type,
+  name,
+  latitude_deg,
+  longitude_deg,
+  elevation_ft,
+  continent,
+  iso_country,
+  iso_region,
+  municipality,
+  scheduled_service,
+  gps_code,
+  iata_code,
+  local_code,
+  home_link,
+  wikipedia_link,
+  keywords
+)
+USING {"from": {"file": {"header":"1","columnseparator":","} }}
+GO
+
+DROP TABLE flights.countries 
+GO
+CREATE TABLE flights.countries 
+(
+  id int NOT NULL,
+  code char(2) NOT NULL,
+  name varchar(255) NOT NULL,
+  continent char(2)  NOT NULL,
+  wikipedia_link varchar(1024),
+  keywords varchar(1024),
+  CONSTRAINT id_pk PRIMARY KEY (id)
+)
+GO
+LOAD DATA FROM FILE '/usr/countries.csv' 
+INTO flights.countries
+(
+  id,
+  code,
+  name,
+  continent,
+  wikipedia_link,
+  keywords
+) 
+VALUES 
+(
+  id,
+  code,
+  name,
+  continent,
+  wikipedia_link,
+  keywords
+)
+USING {"from": {"file": {"header":"1","columnseparator":","} }}
+GO
+DROP TABLE flights.airport_freq 
+GO
+CREATE TABLE flights.airport_freq 
+(
+  id int NOT NULL,
+  airport_id int NOT NULL,
+  airport_ident varchar(8) NOT NULL,
+  type varchar(25) NOT NULL,
+  description varchar(255) NOT NULL,
+  frequency_mhz DOUBLE  NOT NULL,
+  CONSTRAINT id_pk PRIMARY KEY (id)
+)
+GO
+LOAD DATA FROM FILE '/usr/airport_freq.csv' 
+INTO flights.airport_freq
+(
+  id,
+  airport_id,
+  airport_ident,
+  type,
+  description,
+  frequency_mhz
+) 
+VALUES 
+(
+  id,
+  airport_ref,
+  airport_ident,
+  type,
+  description,
+  frequency_mhz
+)
+USING {"from": {"file": {"header":"1","columnseparator":","} }}
+GO
+DROP TABLE flights.regions 
+GO
+CREATE TABLE flights.regions 
+(
+  id int NOT NULL,
+  code varchar(8) NOT NULL,
+  local_code varchar(4) NOT NULL,
+  name varchar(255) NOT NULL,
+  continent char(2) NOT NULL,
+  iso_country char(2) NOT NULL,
+  wikipedia_link varchar(1024),
+  keywords varchar(1024),
+  CONSTRAINT id_pk PRIMARY KEY (id)
+)
+GO
+LOAD DATA FROM FILE '/usr/regions.csv' 
+INTO flights.regions
+(
+  id,
+  code,
+  local_code,
+  name,
+  continent,
+  iso_country,
+  wikipedia_link,
+  keywords
+) 
+VALUES 
+(
+  id,
+  code,
+  local_code,
+  name,
+  continent,
+  iso_country,
+  wikipedia_link,
+  keywords
+)
+USING {"from": {"file": {"header":"1","columnseparator":","} }}
+GO
+
+DROP TABLE flights.runways
+GO
+CREATE TABLE flights.runways 
+(
+  id            INT NOT NULL,
+  airport_id    INT NOT NULL,
+  airport_ident VARCHAR(8) NOT NULL,
+  length_ft     INT,
+  width_ft      INT,
+  surface       TEXT,
+  lighted       BIT,
+  closed        BIT,
+  le_ident      VARCHAR(6),
+  le_latitude_deg DOUBLE,
+  le_longitude_deg DOUBLE,
+  le_location   DOUBLE,
+  le_elevation_ft INT,
+  le_heading_degt DOUBLE,
+  le_displaced_threshold_ft INT,
+  he_ident      VARCHAR(6),
+  he_latitude_deg DOUBLE,
+  he_longitude_deg DOUBLE,
+  he_location   DOUBLE,
+  he_elevation_ft INT,
+  he_heading_degt DOUBLE,
+  he_displaced_threshold_ft INT,
+  CONSTRAINT id_pk PRIMARY KEY (id)
+)
+GO
+LOAD DATA FROM FILE '/usr/runways.csv' 
+INTO flights.runways
+(
+id
+,airport_id
+,airport_ident
+,length_ft
+,width_ft
+,surface
+,lighted
+,closed
+,le_ident
+,le_latitude_deg
+,le_longitude_deg
+,le_elevation_ft
+,le_heading_degT
+,le_displaced_threshold_ft
+,he_ident
+,he_latitude_deg
+,he_longitude_deg
+,he_elevation_ft
+,he_heading_degT
+,he_displaced_threshold_ft
+) 
+VALUES 
+(
+id
+,airport_ref
+,airport_ident
+,length_ft
+,width_ft
+,surface
+,lighted
+,closed
+,le_ident
+,le_latitude_deg
+,le_longitude_deg
+,le_elevation_ft
+,le_heading_degT
+,le_displaced_threshold_ft
+,he_ident
+,he_latitude_deg
+,he_longitude_deg
+,he_elevation_ft
+,he_heading_degT
+,he_displaced_threshold_ft
+)
+USING {"from": {"file": {"header":"1","columnseparator":","} }}
+GO
+DROP TABLE flights.navaids
+GO
+CREATE TABLE flights.navaids 
+(
+  id            INT NOT NULL,
+  filename      varchar(255),
+  ident         varchar(255),
+  name          varchar(255),
+  TYPE          varchar(8),
+  frequency_khz INT,
+  latitude_deg  DOUBLE,
+  longitude_deg DOUBLE,
+  location      DOUBLE,
+  elevation_ft  INT,
+  iso_country   varchar(255),
+  dme_frequency_khz DOUBLE,
+  dme_channel   varchar(255),
+  dme_latitude_deg DOUBLE,
+  dme_longitude_deg DOUBLE,
+  dme_elevation_ft INT,
+  slaved_variation_deg DOUBLE,
+  magnetic_variation_deg DOUBLE,
+  usagetype     varchar(8),
+  POWER         varchar(8),
+  associated_airport VARCHAR(8)
+)
+GO
+LOAD DATA FROM FILE '/usr/navaids.csv' 
+INTO flights.navaids
+(
+ id
+,filename
+,ident
+,name
+,type
+,frequency_khz
+,latitude_deg
+,longitude_deg
+,elevation_ft
+,iso_country
+,dme_frequency_khz
+,dme_channel
+,dme_latitude_deg
+,dme_longitude_deg
+,dme_elevation_ft
+,slaved_variation_deg
+,magnetic_variation_deg
+,usageType
+,power
+,associated_airport
+) 
+VALUES 
+(
+id
+,filename
+,ident
+,name
+,type
+,frequency_khz
+,latitude_deg
+,longitude_deg
+,elevation_ft
+,iso_country
+,dme_frequency_khz
+,dme_channel
+,dme_latitude_deg
+,dme_longitude_deg
+,dme_elevation_ft
+,slaved_variation_deg
+,magnetic_variation_deg
+,usageType
+,power
+,associated_airport
+)
+USING {"from": {"file": {"header":"1","columnseparator":","} }}
+GO
+/*
+Name 		Full name of the aircraft.
+IATA code 	Unique three-letter IATA identifier for the aircraft.
+ICAO code 	Unique four-letter ICAO identifier for the aircraft.
+*/
+DROP TABLE flights.planes
+GO
+CREATE TABLE flights.planes 
+(
+  name varchar(255) NOT NULL,
+  iata varchar(3)  NULL,
+  icao varchar(4)  NULL
+)
+GO
+LOAD DATA FROM FILE '/usr/planes.dat' 
+COLUMNS 
+(
+  name     varchar(255),
+  iata     varchar(3),
+  icao     varchar(4)
+)
+INTO flights.planes
+(
+  name,
+  iata,
+  icao
+) 
+VALUES 
+(
+  name,
+  iata,
+  icao
+)
+USING {"from": {"file": {"header":"0","columnseparator":","} }}
+GO
+
+/*
+Airline 					2-letter (IATA) or 3-letter (ICAO) code of the airline.
+Airline ID 				Unique OpenFlights identifier for airline (see Airline).
+Source airport 			3-letter (IATA) or 4-letter (ICAO) code of the source airport.
+Source airport ID 			Unique OpenFlights identifier for source airport (see Airport)
+Destination airport 		3-letter (IATA) or 4-letter (ICAO) code of the destination airport.
+Destination airport ID 	Unique OpenFlights identifier for destination airport (see Airport)
+Codeshare 				"Y" if this flight is a codeshare (that is, not operated by Airline, but another carrier), empty otherwise.
+Stops 					Number of stops on this flight ("0" for direct)
+Equipment 				3-letter codes for plane type(s) generally used on this flight, separated by spaces
+*/
+DROP TABLE flights.routes
+GO
+CREATE TABLE flights.routes 
+(
+  airline           char(2) NOT NULL,
+  airline_id        int NOT NULL,
+  source_airport    char(3) NOT NULL,
+  source_airport_id INT NOT NULL,
+  dest_airport      char(3) NOT NULL,
+  dest_airport_id   INT NOT NULL,
+  codeshare         varchar(1) null,
+  stops             int not null,
+  equipment        varchar(255) null
+)
+GO
+LOAD DATA FROM FILE '/usr/routes.dat' 
+COLUMNS 
+(
+  airline           char(2),
+  airline_id        int,
+  source_airport    char(3),
+  source_airport_id INT,
+  dest_airport      char(3),
+  dest_airport_id   INT,
+  codeshare         varchar(1),
+  stops             int,
+  equipment        varchar(255)
+)
+INTO flights.routes
+(
+  airline,
+  airline_id,
+  source_airport,
+  source_airport_id,
+  dest_airport,
+  dest_airport_id,
+  codeshare,
+  stops,
+  equipment
+) 
+VALUES 
+(
+  airline,
+  airline_id,
+  source_airport,
+  source_airport_id,
+  dest_airport,
+  dest_airport_id,
+  codeshare,
+  stops,
+  equipment
+)
+USING {"from": {"file": {"header":"0","columnseparator":","} }}
